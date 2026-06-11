2a6c9aff6f121b289b0b611dd9abfceffc7d60c0
Itsme-Meenakshi <msmeenakshisanthosh@gmail.com>
2026-06-11 15:59:07 +0530

    Increase max_tokens limit to 4000

--- api/generate.js
+++ api/generate.js
@@ -18,7 +18,7 @@ export default async function handler(req, res) {
       },
       body: JSON.stringify({
         model: 'llama-3.3-70b-versatile',
-        max_tokens: 1200,
+        max_tokens: 4000,
         messages: [
           {
             role: 'system',
